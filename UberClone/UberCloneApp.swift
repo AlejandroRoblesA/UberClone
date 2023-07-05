ba8ab0b1638f0a3804c93b576081a71fae497cbf
Alejandro Robles <alejandro_r1704@hotmail.com>
2023-07-05 16:05:37 -0600

    Project created

---
+++ UberClone/UberCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberCloneApp.swift
+//  UberClone
+//
+//  Created by Alejandro Robles on 05/07/23.
+//
+
+import SwiftUI
+
+@main
+struct UberCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
