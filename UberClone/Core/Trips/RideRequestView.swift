7ffb601ac4b14450939a211b653bd2f3a0ebfc41
Alejandro Robles <alejandro_r1704@hotmail.com>
2023-07-28 22:06:14 -0600

    Ride Request Header

--- UberClone/Core/Trips/RideRequestView.swift
+++ UberClone/Core/Trips/RideRequestView.swift
@@ -9,7 +9,46 @@ import SwiftUI
 
 struct RideRequestView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Capsule()
+                .foregroundColor(Color(.systemGray5))
+                .frame(width: 48, height: 6)
+            
+            HStack {
+                VStack {
+                    Circle()
+                        .fill(Color(.systemGray3))
+                        .frame(width: 8, height: 8)
+                    Rectangle()
+                        .fill(Color(.systemGray3))
+                        .frame(width: 1, height: 32)
+                    Rectangle()
+                        .fill(Color(.black))
+                        .frame(width: 8, height: 8)
+                }
+                VStack(alignment: .leading, spacing: 24) {
+                    HStack {
+                        Text("Current Location")
+                            .font(.system(size: 16))
+                            .foregroundColor(.gray)
+                        Spacer()
+                        Text("1:30 PM")
+                            .font(.system(size: 14, weight: .semibold))
+                            .foregroundColor(.gray)
+                    }
+                    .padding(.bottom, 10)
+                    HStack {
+                        Text("Starbucks Coffe")
+                            .font(.system(size: 16))
+                            .foregroundColor(.gray)
+                        Spacer()
+                        Text("1:45 PM")
+                            .font(.system(size: 14, weight: .semibold))
+                            .foregroundColor(.gray)
+                    }
+                }
+            }
+        }
     }
 }
 
