c0127cb73f947851060367b553c2bc822c9eb6e3
Alejandro Robles <alejandro_r1704@hotmail.com>
2023-07-30 19:32:23 -0600

    Ride Type Model

---
+++ UberClone/Models/RideType.swift
@@ -0,0 +1,31 @@
+//
+//  RideType.swift
+//  UberClone
+//
+//  Created by Alejandro Robles on 30/07/23.
+//
+
+import Foundation
+
+enum RideType: Int, CaseIterable, Identifiable {
+    case uberX
+    case black
+    case uberXL
+    
+    var id: Int { return rawValue }
+    var description: String {
+        switch self {
+        case .uberX: return "UberX"
+        case .black: return "UberBlack"
+        case .uberXL: return "UberXL"
+        }
+    }
+    
+    var imageName: String {
+        switch self {
+        case .uberX: return "uber-x"
+        case .black: return "uber-black"
+        case .uberXL: return "uber-xl"
+        }
+    }
+}
