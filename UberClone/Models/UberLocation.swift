f8d23ad2f11f60ee4da4edd9a54b7bfd10cea996
Alejandro Robles <alejandro_r1704@hotmail.com>
2023-08-10 12:22:39 -0600

    UberLocation Model

---
+++ UberClone/Models/UberLocation.swift
@@ -0,0 +1,13 @@
+//
+//  UberLocation.swift
+//  UberClone
+//
+//  Created by Alejandro Robles on 10/08/23.
+//
+
+import CoreLocation
+
+struct UberLocation {
+    let title: String
+    let coordinate: CLLocationCoordinate2D
+}
